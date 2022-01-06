fc09961fb77c6e05284b9a1735f93efa80738552
Dayron <daelchinove@gmail.com>
2022-01-06 14:28:38 -0500

    Hice los id auto_increments con la palabra INTEGER

--- jugando_SQLite.py
+++ jugando_SQLite.py
@@ -3,20 +3,21 @@ import sqlite3
 connection = sqlite3.connect('data.bd')
 cursor = connection.cursor()
 
-create_table = "create table items (id integer primary key, name text, password real)"
+create_table = "create table items (id INTEGER primary key, name text, price real)"
+# Al usar la palabra INTEGER (con mayuscula) hace los id auto_increments
 cursor.execute(create_table)
 
 # Insertar uno
-item = (1, 'pelota', 6.12)
-insert_query = "insert into items values (?,?,?)"
+item = ('pelota', 6.12)
+insert_query = "insert into items (name, price) values (?,?)"
 cursor.execute(insert_query, item)
 
 
 # Insertar varios items
 items = [
-    (2, 'bicicleta', 63.56),
-    (3, 'laptop', 302.64),
-    (4, 'audifonos', 12.99)
+    ('bicicleta', 63.56),
+    ('laptop', 302.64),
+    ('audifonos', 12.99)
 ]
 cursor.executemany(insert_query, items)
 
