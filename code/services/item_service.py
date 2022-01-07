a320bd841bf3e0e549b55574fb97c7ed2aeed935
Dayron <daelchinove@gmail.com>
2022-01-07 08:55:10 -0500

    Me gusta mas asi (con las excepciones en el controller pero si las quiero en el service estan en el commit anterior)

--- code/services/item_service.py
+++ code/services/item_service.py
@@ -8,24 +8,24 @@ class ItemService:
 
     @staticmethod
     def get_by_id(id):
-        item = ''
-        try:
-            # connection = sqlite3.connect('dat.bd') Probe la excepcion asi
-            connection = sqlite3.connect('data.bd')
-            cursor = connection.cursor()
+        # item = ''
+        # try:
+        # connection = sqlite3.connect('dat.bd')  # Probe la excepcion asi
+        connection = sqlite3.connect('data.bd')
+        cursor = connection.cursor()
 
-            query = f"select * from {ItemService.TABLE_NAME} where id = ?"
-            # Siempre tiene que ser una tupla
-            result = cursor.execute(query, (id,))
-            row = result.fetchone()
+        query = f"select * from {ItemService.TABLE_NAME} where id = ?"
+        # Siempre tiene que ser una tupla
+        result = cursor.execute(query, (id,))
+        row = result.fetchone()
 
-            # item = Item(row[0], row[1], row[2]) if row else None
-            item = Item(*row) if row else None
-        except Exception as error:
-            raise Exception("Error interno")
-        finally:
-            # No hace falta el commit porque no agregamos ningun dato
-            connection.close()
+        # item = Item(row[0], row[1], row[2]) if row else None
+        item = Item(*row) if row else None
+        # except Exception as error:
+        #     raise Exception("Error interno")
+        # finally:
+        # No hace falta el commit porque no agregamos ningun dato
+        connection.close()
         return item.json() if item else None
 
     @staticmethod
