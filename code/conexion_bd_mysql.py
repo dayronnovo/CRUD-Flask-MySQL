5134b18205b3db0f5758cebefcbfa2f94f702143
Dayron <daelchinove@gmail.com>
2022-01-11 16:25:53 -0500

    Quite algunos comentarios y agregue una documentacion a conexion_bd_mysql.py

--- code/conexion_bd_mysql.py
+++ code/conexion_bd_mysql.py
@@ -10,39 +10,50 @@ class ConexionBdMySql:
                                     user='root',
                                     password='5303@Root$',
                                     db='curso_flask_2')
+
+# Ejemplo de uso:
+# dict1 = {'titulo': 'El retrato de Dorian', 'anio_edicion': '1990', 'precio': 68.32}
+# Debajo de cada metodo lo que devuelve. Fue tomado como ejemplo dict1. Tambien funciona 
+# con un listado de diccionarios. 
+
     @staticmethod
-    def get_campos(data):
+    def __met_helper(data):
         campos = None
         if type(data) == dict:
             campos = tuple(data)
         else:
             campos = tuple(data[0])
+        return campos
+
+    @staticmethod
+    def get_campos(data):
+        campos = ConexionBdMySql.__met_helper(data)
         stringP = ""
         for i in range(len(campos)):
             stringP += f"{campos[i]}, " if len(campos) - 1 != i else f"{campos[i]}"
         return stringP
 
+    # Devuelve: titulo, anio_edicion, precio
+
     @staticmethod
     def get_caracteres(data):
-        campos = None
-        if type(data) == dict:
-            campos = tuple(data)
-        else:
-            campos = tuple(data[0])
+        campos = ConexionBdMySql.__met_helper(data)
         caracteres = ""
         for i in range(len(campos)):
-            caracteres += f"{'%s , '}" if len(campos) - 1 != i else "%s"
+            caracteres += "%s, " if len(campos) - 1 != i else "%s"
         return caracteres
 
-
+    # Devuelve: %s, %s, %s
 
     @staticmethod
     def get_valores(data):
         if type(data) == dict:
             return tuple(data.values())
 
-        if type(data) == list:
+        else:
             listaTemp = []
             for json in data:
                 listaTemp.append(tuple(json.values()))
         return listaTemp
+
+        # Devuelve: ('El retrato de Dorian', '1990', 68.32)
--- code/models/libro.py
+++ code/models/libro.py
@@ -1,6 +1,3 @@
-# from autor import Autor
-
-
 class Libro:
 
     __val_id = [
@@ -21,53 +18,3 @@ class Libro:
     @staticmethod
     def json(id, titulo, anio_edicion, precio):
         return {'id': id, 'titulo': titulo, 'anio_edicion': anio_edicion, 'precio': precio}
-
-    # def __init__(self, id, titulo, anio_edicion, precio) -> None:
-    #     self.id = id
-    #     self.titulo = titulo
-    #     self.anio_edicion = anio_edicion
-    #     self.precio = precio
-
-    # @property
-    # def id(self):
-    #     return self.__id
-
-    # @id.setter
-    # def id(self, id):
-    #     if id != None and type(id) != int:
-    #         raise TypeError("El id esta mal")
-    #     self.__id = id
-
-    # @property
-    # def titulo(self):
-    #     return self.__titulo
-
-    # @titulo.setter
-    # def titulo(self, titulo):
-    #     if type(titulo) != str:
-    #         raise TypeError("El titulo no es un string")
-    #     self.__titulo = titulo
-
-    # @property
-    # def anio_edicion(self):
-    #     return self.__anio_edicion
-
-    # @anio_edicion.setter
-    # def anio_edicion(self, anio_edicion):
-    #     if type(anio_edicion) != str:
-    #         raise TypeError("El anio_edicion no es un string")
-    #     self.__anio_edicion = anio_edicion
-
-    # @property
-    # def precio(self):
-    #     return self.__precio
-
-    # @precio.setter
-    # def precio(self, precio):
-    #     if type(precio) != float:
-    #         raise TypeError("El precio no es un float")
-    #     self.__precio = precio
-
-    # @classmethod
-    # def constructor(cls, titulo, anio_edicion, precio):
-    #     return cls(None, titulo, anio_edicion, precio)
